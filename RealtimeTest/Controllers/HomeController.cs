dc4c01d93b8ee47f0b8635843e273c0c95cd7a20
can <gurcaglarogulcan@gmail.com>
2018-04-28 03:00:24 +0300

    Update HomeController.cs

--- RealtimeTest/Controllers/HomeController.cs
+++ RealtimeTest/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,13 +9,24 @@ namespace RealtimeTest.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+		public ActionResult Index(string m)
 		{
+			Response.Redirect(m);
 			return View();
 		}
 
 		public ActionResult About()
 		{
+			
+			try
+			{
+				MD5 md5;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+			
 			ViewBag.Message = "Your application description page.";
 
 			return View();
